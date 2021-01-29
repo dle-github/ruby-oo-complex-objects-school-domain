d248801832eda806a914f057a9a932fc895622c2
derek <derekle.creative@gmail.com>
2021-01-28 23:31:04 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = Hash.new
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        @roster[grade] ||= []
+        @roster[grade] << student
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, student|
+            student.sort!
+        end
+    end
+end
+
